1671a5a0038f25fded466538459c1e472150dde2
sk824910 <s.keashon@gmail.com>
2022-06-30 22:11:08 -0400

    adding a basic todo app

--- apps/my-fullstack-app-e2e/src/integration/app.spec.ts
+++ apps/my-fullstack-app-e2e/src/integration/app.spec.ts
@@ -1,13 +1,12 @@
-import { getGreeting } from '../support/app.po';
+import { getAddTodoButton, getTodos } from '../support/app.po';
 
 describe('my-fullstack-app', () => {
   beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome my-fullstack-app');
+  it('should display todos', () => {
+    getTodos().should((t) => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodos().should((t) => expect(t.length).equal(3));
   });
 });
--- apps/my-fullstack-app/src/app/app.component.ts
+++ apps/my-fullstack-app/src/app/app.component.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@myworkspace/api-interfaces';
 
+interface Todo {
+  title: string;
+}
+
+
+
 @Component({
   selector: 'myworkspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  todos: Todo[] = [{title: 'Todo 1'}, { title: 'Todo 2'}]
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
+  
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  constructor(private http: HttpClient) {
+    this.fetch()
+  }
 }
